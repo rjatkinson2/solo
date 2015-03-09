eca1f2400f3bed85701f28b908853045757461ea
Ryan Atkinson <rjatkinson2@gmail.com>
2015-03-09 11:47:11 -0700

    Added myTunes backbone structure as starting point for the app.

---
+++ client/views/LibraryView.js
@@ -0,0 +1,21 @@
+// LibraryView.js - Defines a backbone view class for the music library.
+var LibraryView = Backbone.View.extend({
+
+  tagName: "table",
+
+  initialize: function() {
+
+    this.render();
+  },
+
+  render: function() {
+    this.$el.children().detach();
+
+    return this.$el.html('<th>Library</th>').append(
+      this.collection.map(function(song){
+        return new LibraryEntryView({model: song}).render();
+      })
+    );
+  }
+
+});
---
+++ client/views/PlayerView.js
@@ -0,0 +1,24 @@
+// PlayerView.js - Defines a backbone view class for the music player.
+var PlayerView = Backbone.View.extend({
+
+  // HTML5 (native) audio tag is being used
+  // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
+  el: '<audio controls autoplay />',
+
+  initialize: function() {
+    this.$el.on('ended', (function () { this.model.ended() }).bind(this) );
+  },
+
+  setSong: function(song){
+    this.model = song;
+    if(!this.model.get('url')){
+      this.el.pause();
+    }
+    this.render();
+  },
+
+  render: function(){
+    return this.$el.attr('src', this.model.get('url') || '');
+  }
+
+});
